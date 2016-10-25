96d0a33b6fac35a9fbcfc01f0121bf13886cf897
林志杰 <18814103637@163.com>
2016-10-25 09:48:52 +0800

    添加UserDaoImpl测试类

---
+++ src/test/java/com/express/DAO/daoImp/UserDaoImpTest.java
@@ -0,0 +1,85 @@
+package test.com.express.DAO.daoImp; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* UserDaoImp Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Ê®ÔÂ 24, 2016</pre> 
+* @version 1.0 
+*/ 
+public class UserDaoImpTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getUserByPhone(String phone) 
+* 
+*/ 
+@Test
+public void testGetUserByPhone() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getAllUser() 
+* 
+*/ 
+@Test
+public void testGetAllUser() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addUser(User user) 
+* 
+*/ 
+@Test
+public void testAddUser() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: delUser(String id) 
+* 
+*/ 
+@Test
+public void testDelUser() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: updUser(User user) 
+* 
+*/ 
+@Test
+public void testUpdUser() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: main(String arg[]) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
