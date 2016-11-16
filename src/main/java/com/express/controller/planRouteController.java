eee9c9d7a1af555800138039d86a756cd065b8ba
林志杰 <18814103637@163.com>
2016-11-16 12:03:02 +0800

    规划路线控制器：planRouteController

---
+++ src/main/java/com/express/controller/planRouteController.java
@@ -0,0 +1,13 @@
+package com.express.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by linzhijie on 2016/11/16.
+ */
+@Controller
+public class planRouteController {
+
+    @Autowired
+}
